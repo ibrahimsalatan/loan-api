428f198b537c8738a14ff0f3953895da9b34a0ef
ibrahimsalatan <ibrahimsalatan.gmail.com>
2025-01-19 18:04:23 +0300

    first commit

---
+++ src/test/java/com/abank/loanapi/LoanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abank.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
